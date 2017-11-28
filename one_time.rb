adc8b914efa394577a9da4ced869c1cfbb2add29
rob <(nothinghere)>
2017-11-27 21:50:58 -0800

    A few tweaks to make it cleaner.

--- one_time.rb
+++ one_time.rb
@@ -1,10 +1,12 @@
 # One-time pad
 class OneTimePad
 
+   # Strip out all special characters and numbers
   def clean_string(s);
-    s.upcase.delete("^a-zA-Z")  # Strip out all special characters and numbers
+    s.upcase.delete("^a-zA-Z")
   end
 
+  # Make key of random chars of length "len".
   def make_key(len)
     Array.new(len) { rand(26) }
   end
@@ -12,7 +14,7 @@ class OneTimePad
   def encrypt(plaintext, key)
     enc=[]
     plaintext=clean_string(plaintext)
-    puts "Encrypting plaintext => #{plaintext}"
+    return false if plaintext.length>key.length  # ok if key is greater or equal
     pt=plaintext.chars.map { |ch| ch.ord-65 }
     pt.each_with_index { |p,i| enc[i] = (((p+key[i]) % 26)+65).chr }
     enc.join
@@ -29,8 +31,11 @@ end
 
 c=OneTimePad.new
 plaintext="hello world"
-key=c.make_key(plaintext.length)
+k=c.clean_string(plaintext)
+key=c.make_key(k.length)
 puts "result key=#{key}"
+puts "plaintxt= #{k}"
+puts "key map = #{ key.map {|x| (x.ord+65).chr }.join }"
 enc=c.encrypt(plaintext, key )
 puts "encrypt = #{enc}"
 dec=c.decrypt(enc,key )
